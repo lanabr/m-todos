6dbad3c62a6068f7524bf3819965a54f7b42f3b2
lanabr <lanarossato@hotmail.com>
2018-11-14 20:21:51 -0200

    esse ta de primeira

---
+++ newton.py
@@ -0,0 +1,38 @@
+import numpy as np
+
+def f(x):  #f = x^4 - 2x^3 - 4x^2 + 4x + 4:
+    total = pow(x, 4) - 2 * pow(x, 3) - 4 * pow(x, 2) + 4 * x + 4
+    return total
+
+def deri(x): #f' = 4x^3 - 6x^2 - 8x + 4
+    total = 4 * pow(x, 3) - 6 * pow(x, 2) - 8 * x + 4
+    return total
+
+def newton(e, max, a, b):
+    xf = 0
+    x = []
+    i = 0
+    x.append((a + b) / 2)
+
+    for k in range(0, max):
+        x.append(x[k] - f(x[k]) / deri(x[k]))
+
+        erro = np.abs(x[k] - x[k - 1])
+        print(i, x[k], erro)
+
+        if erro < e:
+            xf = x[k]
+            break
+
+        i = i + 1
+
+def main():
+    #e = input("Erro: ")
+    #max = input("Max iterações: ")
+    #a = input("Ponto a: ")
+    #b = input("Ponto b: ")
+    #newton(float(e), int(max), int(a), int(b))
+    newton(0.0000001, 20, 0, 3)
+
+if __name__ == '__main__':
+    main()
