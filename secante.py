d37c68f260550155d6723335eb36351f59925a9a
lanabr <lanarossato@hotmail.com>
2018-11-14 23:03:23 -0200

    errrou

--- secante.py
+++ secante.py
@@ -11,7 +11,8 @@ def secante(e, max, a, b):
     x.append((a + b) / 2)
     print("0", x[0], "0.0")
     x.append(((a + b) / 2) - 0.1)
-    i = 1
+    print("1", x[1], np.abs(x[1] - x[0]))
+    i = 2
     for k in range(2, max):
 
         x.append((x[k - 2] * f(x[k - 1]) - x[k - 1] * f(x[k - 2])) / (f(x[k - 1]) - f(x[k - 2])))
