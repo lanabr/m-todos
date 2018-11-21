6a236df427b0d00223c99e914d5f4a57bdba1412
lanabr <lanarossato@hotmail.com>
2018-11-21 19:40:20 -0200

    isso não funciona

--- biseccao.py
+++ biseccao.py
@@ -1,6 +1,8 @@
 import numpy as np
 
 def f(x): #f = x^4 - 2x^3 - 4x^2 + 4x + 4:
+    #total = pow(x, 3) + 4 * pow(x, 2) - 10 #to testando com essa
+    #print("Total:", total) #debug
     total = pow(x, 4) - 2 * pow(x, 3) - 4 * pow(x, 2) + 4 * x + 4
     return total
 
@@ -9,8 +11,9 @@ def bisec(e, max, a, b):   #e = precisão
     x = []
 
     for k in range(0, max):
-       x.append((a + b) / 2)
-
+        x.append((a + b) / 2)
+        #print("a:", a,"b:", b) #debug
+        
         if (f(a) * f(b)) < 0:
             b = x[k]
         else:
@@ -18,9 +21,9 @@ def bisec(e, max, a, b):   #e = precisão
 
         erro = np.abs(x[k] - x[k - 1])
         print(i, x[k], erro)
+        #print("--------------") #debug
 
         if np.abs(b - a) < e:
-            x.append((b - a)/2)
             break
 
         i = i + 1
@@ -31,7 +34,12 @@ def main():
     #a = input("Ponto a: ")
     #b = input("Ponto b: ")
     #bisec(float(e), int(max), int(a), int(b))
-    bisec(0.0000001, 10, 0, 3)
+    #bisec(0.0001, 20, 1, 2) #testeee
+    bisec(0.000001, 10, 0, 3)
+    
+    k = (np.log10(3) - np.log10(0.000001)) / np.log10(2)
+    print("Quantidade de iterações teóricas:", np.ceil(k))
+
 
 if __name__ == '__main__':
     main()
