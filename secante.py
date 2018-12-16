069e6838f71b5327ae21d023f3b57165ee436dfe
lanabr <lanarossato@hotmail.com>
2018-12-16 21:30:18 -0200

    agora vai

--- secante.py
+++ secante.py
@@ -1,19 +1,18 @@
 import numpy as np
 
-def f(x):  #f = x^4 - 2x^3 - 4x^2 + 4x + 4:
-    total = pow(x, 4) - 2 * pow(x, 3) - 4 * pow(x, 2) + 4 * x + 4
+def f(x):  #f = x^4 - 2x^3 - 4x^2 + 4x + 4, f' = 4x^3 - 6x^2 - 8x + 4
+    total = 4 * pow(x, 3) - 6 * pow(x, 2) - 8 * x + 4
     return total
 
 def secante(e, max, a, b):
     x = []
 
-    x.append((a + b) / 2)  #+ 1 para o segundo ponto
+    x.append((a + b) / 2) 
     print("0", x[0], "0.0")
-    x.append(((a + b) / 2) - 0.1) #+ 0.9 para o segundo ponto
+    x.append(((a + b) / 2) - 0.1) 
     print("1", x[1], np.abs(x[1] - x[0]))
     i = 2
     for k in range(2, max):
-
         x.append((x[k - 2] * f(x[k - 1]) - x[k - 1] * f(x[k - 2])) / (f(x[k - 1]) - f(x[k - 2])))
 
         erro = np.abs(x[k] - x[k - 1])
